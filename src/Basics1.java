4d1e2e7d64d47dc8018c3f69336346c9a7061168
payalalag <payalalag28@gmail.com>
2019-01-07 00:29:37 +0530

    First commit

---
+++ src/Basics1.java
@@ -0,0 +1,14 @@
+
+public class Basics1 {
+
+	public static void main(String[] args) 
+	{
+		// Add 2 numbers
+		int x=2;
+		int y=5;
+		int Sum = x+y;
+		System.out.println("Sum is : " + Sum);
+
+	}
+
+}
